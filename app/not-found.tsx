6e7dbafc2b0e5a4a2f37da22a4a02214a54b10f1
samzero <88150335+samziro@users.noreply.github.com>
2026-01-15 16:41:54 +0300

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen text-center px-4">
       <h1 className="text-5xl md:text-5xl font-semibold text-white">404</h1>
       
-      <p className="text-slate-100 mt-4">Please check the URL or return to the homepage.</p>
+      <p className="text-textSecondary mt-4">Please check the URL or return to the homepage.</p>
       <Link
         href="/"
         className="mt-6 inline-block bg-yellow-600 text-white px-6 py-3  hover:bg-yellow-700 transition-colors"
